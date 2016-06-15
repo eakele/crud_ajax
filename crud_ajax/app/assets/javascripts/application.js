31975dc8bbf2d3d467167cfab8e56f1703cb3a19
Ephrem <eakele@gmail.com>
2016-06-15 18:27:00 +1000

    single page app

--- crud_ajax/app/assets/javascripts/application.js
+++ crud_ajax/app/assets/javascripts/application.js
@@ -14,3 +14,42 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require materialize-sprockets
+
+$(document).ready(function(){
+  $('.modal-trigger').leanModal();
+
+  $('.modal-content form').on('submit', function(e){
+    e.preventDefault(); // To stop the submit button from working in ruby/rails
+    var data = $(this).serialize();
+    $.post('/todos', data, function(data){
+      console.log(data);
+      $('#modal1').closeModal();
+
+      var tr = $('<tr>');
+      var td = tr.append($('<td>').text(data.done),$('<td>').text(data.priority),$('<td>').text(data.task));
+      $('tbody').append(td);
+
+      // $('tbody').append($('<tr>'));
+      // $('tbody tr:last-child').append($('<td>').text(data.done));
+      // $('tbody tr:last-child').append($('<td>').text(data.priority));
+      // $('tbody tr:last-child').append($('<td>').text(data.task));
+
+    })
+  });
+
+  $('.button_to').on('submit', function(e){
+    e.preventDefault();
+    var todosId = $(this).attr('id'); //Passing id of todo to ruby routes
+    console.log(todosId);
+    $.ajax({
+      url: '/todos/'+todosId,
+      method: 'DELETE',
+      success: function(data){
+        $('#'+todosId).parent().parent().remove();
+      }
+    })
+  })
+
+
+});
---
+++ crud_ajax/app/controllers/todos_controller.rb
@@ -0,0 +1,9 @@
+class TodosController < ApplicationController
+  def create
+    todo = Todo.create(todo_params)
+    render json: todo.to_json
+  end
+  def todo_params
+    params.require(:todo).permit(:task)
+  end
+end
